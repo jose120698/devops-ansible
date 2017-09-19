543236e5bb674ebcb781b65c7b4bf92c851906e8
Travis Rowland <travis.rowland@gmail.com>
2017-09-19 09:43:58 -0700

    Initial commit

---
+++ vagrant-libs/bootstrap.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+echo '### Install Ansible if not already existing ###'
+yum -y install ansible
+
+echo '### Install python virtualenv ###'
+pip install virtualenv
+
+# echo '### Generate initial keys for Ansible and deploy locally ###'
+# cd /vagrant; ansible-playbook playbooks/generate_keys.yml
+
+# echo '### Run Ansible ###'
+# cd /vagrant && ansible-playbook site.yml -e "env=vagrant"
