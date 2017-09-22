c1a27bc2b5e55558c030112d01b33f7849df056c
Sergey Maslyakov <sergey.maslyakov@ericsson.com>
2017-09-22 09:22:26 -0500

    Added the basic wrapper toles for Consul Agent, Consul Server, and Traefik Server. No meaningful configuration is implemented yet

--- vagrant-libs/bootstrap.sh
+++ vagrant-libs/bootstrap.sh
@@ -3,8 +3,23 @@
 echo '### Install Ansible if not already existing ###'
 yum -y install ansible
 
+echo '### Update Python pip to the latest ###'
+pip install -U pip
+
 echo '### Install python virtualenv ###'
 pip install virtualenv
+
+echo '### Install Traefik role for Ansible ###'
+ansible-galaxy install kibatic.traefik
+
+echo '### Install Consul role for Ansible ###'
+ansible-galaxy install savagegus.ansible-consul
+
+echo '### Install Vim ###'
+yum -y install vim
+
+echo '### Customize .bashrc for user vagrant ###'
+echo 'alias d="ls -AlF"' >> /home/vagrant/.bashrc
 
 # echo '### Generate initial keys for Ansible and deploy locally ###'
 # cd /vagrant; ansible-playbook playbooks/generate_keys.yml
