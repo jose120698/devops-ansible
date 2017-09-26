65ff5a4218aa61b7fd489ef9ed5c21c56ffe7e79
Travis Rowland <travis.rowland@gmail.com>
2017-09-26 10:04:12 -0700

    Migrating traefik role to conventions

--- vagrant-libs/bootstrap.sh
+++ vagrant-libs/bootstrap.sh
@@ -9,20 +9,8 @@ pip install -U pip
 echo '### Install python virtualenv ###'
 pip install virtualenv
 
-echo '### Install Traefik role for Ansible ###'
-ansible-galaxy install kibatic.traefik
-
-echo '### Install Consul role for Ansible ###'
-ansible-galaxy install savagegus.ansible-consul
-
 echo '### Install Vim ###'
 yum -y install vim
-
-echo '### Customize .bashrc for user vagrant ###'
-echo 'alias d="ls -AlF"' >> /home/vagrant/.bashrc
-
-# echo '### Generate initial keys for Ansible and deploy locally ###'
-# cd /vagrant; ansible-playbook playbooks/generate_keys.yml
 
 # echo '### Run Ansible ###'
 # cd /vagrant && ansible-playbook site.yml -e "env=vagrant"
