4c5ea9c1f4623116e09f6426517e4a61dddce296
Travis Rowland <travis.rowland@gmail.com>
2017-10-01 17:55:44 -0700

    Moving additiona package installations out of bootstrap.sh and into inventory

--- vagrant-libs/bootstrap.sh
+++ vagrant-libs/bootstrap.sh
@@ -3,17 +3,5 @@
 echo '### Install Ansible if not already existing ###'
 yum -y install ansible
 
-echo '### Update Python pip to the latest ###'
-pip install -U pip
-
-echo '### Install python virtualenv ###'
-pip install virtualenv
-
-echo '### Install python docker-py ###'
-pip install docker-py
-
-echo '### Install Vim ###'
-yum -y install vim
-
 # echo '### Run Ansible ###'
 # cd /vagrant && ansible-playbook site.yml -e "env=vagrant"
